6ecdc497353ab0a2ebb6aeffdffa966bf8fbfcdd
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-03-22 13:23:24 -0500

    first commit

---
+++ src/main/java/com/examen/esteban/ExamenfinalestebanbacuilimaApplication.java
@@ -0,0 +1,13 @@
+package com.examen.esteban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenfinalestebanbacuilimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenfinalestebanbacuilimaApplication.class, args);
+	}
+
+}
